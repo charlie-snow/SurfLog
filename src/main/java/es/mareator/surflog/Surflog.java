d410300f0f458e3b5434718079eee60ec460a79f
charlie-snow <victo@victoriano.none>
2024-02-16 14:06:18 +0100

    holasurf

---
+++ src/main/java/es/mareator/surflog/Surflog.java
@@ -0,0 +1,12 @@
+package es.mareator.surflog;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Surflog {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundosurf");
+	}
+
+}
---
+++ src/test/java/es/mareator/surflog/SurflogApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mareator.surflog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurflogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
