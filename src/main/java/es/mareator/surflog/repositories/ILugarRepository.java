0ae3caa945e1103160e0f1056afc5a88abbcd7cb
charlie-snow <victo@victoriano.none>
2024-02-20 13:48:56 +0100

    limpiar

--- src/main/java/es/mareator/surflog/repositories/ILugarRepository.java
+++
@@ -1,9 +0,0 @@
-package es.mareator.surflog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.mareator.surflog.models.LugarModel;
-
-public interface ILugarRepository extends JpaRepository<LugarModel, Long> {
-
-}
--- src/main/java/es/mareator/surflog/repositories/IRegistroRepository.java
+++
@@ -1,9 +0,0 @@
-package es.mareator.surflog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.mareator.surflog.models.RegistroModel;
-
-public interface IRegistroRepository extends JpaRepository<RegistroModel, Long> {
-
-}
